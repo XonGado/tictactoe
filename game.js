60fbf71ff2907c22e2e73f7bd9d87fc80cb31445
Maynard Vargas <MY_NAME@example.com>
2017-10-07 17:30:07 +0800

    With mock game

--- game.js
+++ game.js
@@ -1,37 +1,46 @@
 "use strict";
 
-
+const PLAYER_X  = "x";
+const PLAYER_O  = "o";
+const BLANK     = "_";
 
 
 class Game {
     constructor(){
         this.state = {
-            "topLeft":      "_",
-            "topMiddle":    "_",
-            "topRight":     "_",
-            "centerLeft":   "_",
-            "centerMiddle": "_",
-            "centerRight":  "_",
-            "bottomLeft":   "_",
-            "bottomMiddle": "_",
-            "bottomRight": "_"
+            "topLeft":      BLANK,
+            "topMiddle":    BLANK,
+            "topRight":     BLANK,
+            "centerLeft":   BLANK,
+            "centerMiddle": BLANK,
+            "centerRight":  BLANK,
+            "bottomLeft":   BLANK,
+            "bottomMiddle": BLANK,
+            "bottomRight": BLANK
         }
         this.turnNumber = 1;
-        this.currentPlayer = "x";
+        this.currentPlayer = PLAYER_X;
     }
 
-    move(player, placement){
-        this.state[placement] = player;
+    move(player, position){
+        this.state[position] = player;
         this.changeTurn();
     }
 
     changeTurn(){
-        if(this.currentPlayer == "x"){
-            this.currentPlayer = "o";
+        if(this.currentPlayer == PLAYER_X){
+            this.currentPlayer = PLAYER_O;
         }
         else{
-            this.currentPlayer = "x";
+            this.currentPlayer = PLAYER_X;
         }
         this.turnNumber++;
+    }
+
+    isValidMove(position){
+        if(this.state[position] == BLANK){
+            return true;
+        }
+        return false;
     }
 }
--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,48 @@
+
+
+// view.display(game);
+// game.move("x", "topLeft");
+// view.display(game);
+// game.move("o", "centerMiddle");
+// view.display(game);
+
+
+class TicTacToe{
+    constructor(game, view){
+        this.game = game;
+        this.view = view;
+    }
+
+    playTurn(position){
+        if(this.game.isValidMove(position)){
+            // Human
+            if(this.game.currentPlayer == PLAYER_X){
+                this.game.move(PLAYER_X, position);
+                this.view.display(this.game);
+            }
+            // AI 
+            else{
+                this.game.move(PLAYER_O, position);
+                this.view.display(this.game);
+            }
+        }    
+    }
+}
+
 let game = new Game();
 let view = new View();
 
+const tictactoe = new TicTacToe(game,view);
+setTimeout(function(){
+    tictactoe.playTurn("topRight");
+    setTimeout(function(){
+        tictactoe.playTurn("topLeft");
+        setTimeout(function(){
+            tictactoe.playTurn("centerRight");
+            setTimeout(function(){
+                tictactoe.playTurn("topRight");
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
 
-
-
-
-
-
-view.display(game);
-game.move("x", "topLeft");
-view.display(game);
-game.move("o", "centerMiddle");
-view.display(game);
